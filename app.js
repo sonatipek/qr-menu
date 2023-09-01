a64d5c07c5ccf5c278b380da04cb564c85ad67fd
Sonat Saygın İPEK <sonatipek55@gmail.com>
2023-09-01 19:38:44 +0300

    heroku deploy hatası düzeltildi

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ const path = require('path');
 const userRoutes = require('./routes/user');
 const adminRoutes = require('./routes/admin');
 
-if (process.env.NODE_ENV == "production") {
-    require('./startup/production')(app);
-}
+
 
 // Variables
-const PORT = 3000; 
+const PORT = process.env.PORT || 3000; 
 
 // Create instance
 const app = express()
+
+if (process.env.NODE_ENV == "production") {
+    require('./startup/production')(app);
+}
 
 // Middlewares
 app.use('/libs', express.static(path.join(__dirname, 'node_modules')));
